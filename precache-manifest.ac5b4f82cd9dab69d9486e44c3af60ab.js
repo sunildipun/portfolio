9bba69bd91b15d72ad1ccd8cab2f256ac23ae431
Sunil Behera <sunil@nubewell.com>
2020-07-30 16:00:56 +0530

    Updates

---
+++ precache-manifest.ac5b4f82cd9dab69d9486e44c3af60ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2e92451d5ef65674bf",
+    "url": "/static/css/main.a82694a7.chunk.css"
+  },
+  {
+    "revision": "da2e92451d5ef65674bf",
+    "url": "/static/js/main.da2e9245.chunk.js"
+  },
+  {
+    "revision": "fc22193c0e09b80f7167",
+    "url": "/static/js/1.fc22193c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f52e3bed7497616d1a4400b4105da8a6",
+    "url": "/static/media/unslpash.f52e3bed.jpg"
+  },
+  {
+    "revision": "c0ea9ad58bff37c771456dce2bbc9859",
+    "url": "/index.html"
+  }
+];
